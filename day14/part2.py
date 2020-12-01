788589bf09df15a49d92e8f213a87f5f7c1136c2
r00 <r00@r00s-MacBook-Pro.local>
2020-11-30 23:01:57 -0800

    Setup all days

---
+++ day14/part2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Advent of Code 2020 - Day 14 part 2."""
+
---
+++ day23/part1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Advent of Code 2020 - Day 23 part 1."""
+
