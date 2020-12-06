bedffd4f2f6d9f62fa8a839bd7c304b163cd256a
r00 <r00@r00s-MacBook-Pro.local>
2020-12-05 20:43:45 -0800

    Day 5

--- day05/part1.py
+++ day05/part1.py
@@ -3,3 +3,38 @@
 
 """Advent of Code 2020 - Day 5 part 1."""
 
+def parse_input(file_name: str):
+    with open(file_name, 'r') as f:
+        return [[l for l in board_pass.strip()] for board_pass in f.readlines()]
+
+def calculate_seat_id(row, seat):
+    return (row * 8) + seat
+
+if __name__ == '__main__':
+    all_seats = []
+    for boarding_pass in parse_input('input.txt'):
+        rows = [row for row in range(128)]
+        seats = [seat for seat in range(8)]
+
+        for instruction in boarding_pass:
+            row_length = int(len(rows) / 2)
+            seat_length = int(len(seats) / 2)
+
+            if instruction == 'F':
+                # Remove back half of rows.
+                del rows[row_length:]
+            elif instruction == 'B':
+                # Remove front half of rows.
+                del rows[0:row_length]
+            elif instruction == 'L':
+                del seats[seat_length:]
+            elif instruction == 'R':
+                del seats[0:seat_length]
+
+        seat_id = calculate_seat_id(rows[0], seats[0])
+        all_seats.append(seat_id)
+    
+    all_seats.sort()
+    print(all_seats)
+    highest_seat_id = all_seats[len(all_seats) - 1]
+    print(f'Highest seat id: {highest_seat_id}')
--- day05/part2.py
+++ day05/part2.py
@@ -3,3 +3,60 @@
 
 """Advent of Code 2020 - Day 5 part 2."""
 
+def parse_input(file_name: str):
+    with open(file_name, 'r') as f:
+        return [[l for l in board_pass.strip()] for board_pass in f.readlines()]
+
+def calculate_seat_id(row, seat):
+    return (row * 8) + seat
+
+def find_open_seat(seats):
+    possible_seats = []
+
+    for seat in all_seats:
+        # Ignore first and last seat
+        seat_index = all_seats.index(seat)
+        if seat_index == 0:
+            continue
+        if seat_index == len(all_seats) - 1:
+            continue
+
+        # Seats before and after the current seat.
+        previous_seat  = all_seats[seat_index - 1]
+        next_seat  = all_seats[seat_index + 1]
+
+        # If the previous and next seats dont have an id 1 off from our 
+        # current seat save it.
+        if previous_seat != seat - 1 or next_seat != seat + 1:
+            possible_seats.append(seat)
+
+    # Return the missing seat.
+    return possible_seats[1] - 1
+
+if __name__ == '__main__':
+    all_seats = []
+
+    for boarding_pass in parse_input('input.txt'):
+        rows = [row for row in range(128)]
+        seats = [seat for seat in range(8)]
+
+        for instruction in boarding_pass:
+            row_length = int(len(rows) / 2)
+            seat_length = int(len(seats) / 2)
+
+            if instruction == 'F':
+                del rows[row_length:]
+            elif instruction == 'B':
+                del rows[0:row_length]
+            elif instruction == 'L':
+                del seats[seat_length:]
+            elif instruction == 'R':
+                del seats[0:seat_length]
+
+        seat_id = calculate_seat_id(rows[0], seats[0])
+        all_seats.append(seat_id)
+    
+    all_seats.sort()
+
+    open_seat = find_open_seat(all_seats)
+    print(f'Seat {open_seat} is open.')
